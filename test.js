5f4f2999494411f4725ba833d8e4f8d2503c44f9
Hyoseok Choi <laizim@naver.com>
2013-08-07 17:59:40 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,41 @@
+var neo4j = require('neo4j-js');
+//var application_root = __dirname,
+//    express = require("express"),
+//	path = require("path"),
+//	HTTPStatus = require('http-status');
+
+neo4j.connect('http://210.107.221.101:7474/db/data/', function (err, graph) {
+    if (err)
+        throw err;
+
+    // do something with the graph
+
+    // get node
+    //graph.getNode(10, function (err, node) {
+        //console.log(err ? err : node.data); // print the node's properties
+    //});
+
+var query = [
+    'START n = node(*) MATCH p = n-[r]-n2',
+    'RETURN p',
+    'LIMIT 5'
+];
+
+graph.query(query.join('\n'), { id: 1 }, function (err, results) {
+    if (err) {
+        console.log(err);
+        console.log(err.stack);
+    }
+    else {
+        for (var i = 0; i < results.length; i++) {
+            var relationship = results[i].r;
+            var node = results[i].m;
+
+            // ... do something with the nodes and relationships we just grabbed 
+        }
+
+        console.log(JSON.stringify(results, null, 5 )); // printing may help to visualize the returned structure
+    }
+});
+
+});	
